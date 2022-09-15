9814c5a550333a08caaf159fa01585a975548829
LucasEspindula <lucasespindula4@gmail.com>
2022-09-15 20:30:14 -0300

    refactor login screen

--- app/src/main/java/br/com/dionataferraz/vendas/ProfileActivity.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/ProfileActivity.kt
@@ -14,6 +14,8 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
         binding = ActivityProfileBinding.inflate(layoutInflater)
 
         binding.btSave.setOnClickListener {
